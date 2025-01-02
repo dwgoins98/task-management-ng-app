c076ae599ac5305deb803f9cc0e636f9371e664c
Daniel Goins <dwgoins98@gmail.com>
2025-01-02 14:09:59 -0500

    Learning Angular through a Udemy course

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,  // In Angular 19 and higher, this is default.
+                     // The default is 'false' for Angular 18 and below. This was for module based comonents
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'     // Older versions of Angular use 'styleUrls' instead
+})
+export class HeaderComponent {
+
+}
