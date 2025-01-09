97cfc9bbe075a4ed3ff54c6c3a0da23745322482
Daniel Goins <dwgoins98@gmail.com>
2025-01-09 11:12:12 -0500

    Added a task component that outputs the selected user name

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input} from '@angular/core';
+import { Component, computed, EventEmitter, Input, input, Output} from '@angular/core';
 
 import { dummyUsers } from './dummyUsers';
+
 
 
 
@@ -12,16 +13,27 @@ import { dummyUsers } from './dummyUsers';
 })
 export class UserComponent {
 
+// Decorator method
+  @Input({required: true}) id!:string
   @Input({required: true}) avatar!:string
   @Input({required: true}) name!:string
+  @Output() select = new EventEmitter<string>()
 
+// Signal Approach
+  // avatar = input.required<string>()
+  // name = input.required<string>()
+  // select = output<string>()
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar
+  // })
   
   public get imagePath() : string {
     return 'assets/users/' + this.avatar
   }
   
   onSelectUser() {
-    
+    this.select.emit(this.id)
   }
 
 }
