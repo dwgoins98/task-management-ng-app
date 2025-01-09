1aa46660facf8014dccbdd9fb43b5936dbeb236d
Daniel Goins <dwgoins98@gmail.com>
2025-01-08 22:35:09 -0500

    Added actions to the button and a getter function

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -14,4 +14,14 @@ export class UserComponent {
 
   selectedUser = dummyUsers[randomIndex]
 
+  
+  public get imagePath() : string {
+    return '/assets/users/' + this.selectedUser.avatar
+  }
+  
+  onSelectUser() {
+    const randomIndex: number = Math.floor(Math.random()*dummyUsers.length)
+    this.selectedUser=dummyUsers[randomIndex]
+  }
+
 }
