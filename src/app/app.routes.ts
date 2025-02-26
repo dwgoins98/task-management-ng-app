4157ddfa66b20ad34d33491c07021d1226f71de1
Daniel Goins <dwgoins98@gmail.com>
2025-02-25 22:14:33 -0500

    Completed routing setup and added NotFound component
    - Updated `app.routes.ts` to include new routes for the application.
    - Added a new `NotFound` component to handle 404 errors.
        - Created `not-found.component.css` for styling the NotFound component.
        - Created `not-found.component.html` for the NotFound component template.
        - Created `not-found.component.spec.ts` for unit testing the NotFound component.
        - Created `not-found.component.ts` to define the NotFound component logic.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: NoTaskComponent,
         title: 'Home'
-    }
+    },
+    {
+        path: '**',
+        component: NotFoundComponent,
+      },
 ];
